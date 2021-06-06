f5b7299d76c4c01c4bf5415fbd2fb0cd358fbcc5
imzhyke <punk1313444@gmail.com>
2021-06-06 21:52:42 +0800

    New Files

--- src/onlinecafe/OnlineCafe.java
+++ src/onlinecafe/OnlineCafe.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package onlinecafe;
 
-/**
- *
- * @author jruthie
- */
+import java.awt.*;  
+import java.awt.event.*;  
+
 public class OnlineCafe {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    public static void main(String args[])  
+    {  
+        DialogExample de = new DialogExample();  
+    }  
     
 }
